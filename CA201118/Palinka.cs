98690a6c0f558a79c9579ed2ec4a55c8e5e6a455
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-11-18 12:49:33 +0100

    Add project files.

---
+++ CA201118/Palinka.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA201118
+{
+    class Palinka
+    {
+        private int _alkoholFok;
+        private string _gyumolcs;
+        private int _mennyiseg;
+        private int _keszitesEve;
+        private int _ar;
+        public int AlkoholFok
+        {
+            get { return _alkoholFok; }
+            set
+            {
+                if (value < 30 || value > 87)
+                    throw new Exception("nem leht ennyi fokos egy pálinka");
+                _alkoholFok = value;
+            }
+        }
+        public string Gyumolcs
+        {
+            get { return _gyumolcs; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("nem lehet null");
+                if (value.Length < 3 || value.Length > 20)
+                    throw new Exception("nem lehet ilyen hosszú nevű gyümölcsből");
+                _gyumolcs = value;
+            }
+        }
+        public int Mennyiseg
+        {
+            get { return _mennyiseg; }
+            set
+            {
+                if (value < 0 || value > 50)
+                    throw new Exception("nem lehet ennyi pálinkád");
+                _mennyiseg = value;
+            }
+        }
+        public int KeszitesEve
+        {
+            get => _keszitesEve;
+            set
+            {
+                if (value < 2000 || value > DateTime.Now.Year)
+                    throw new Exception("nem lehet ez a készítés éve");
+                _keszitesEve = value;
+            }
+        }
+        public int Ar
+        {
+            get => _ar;
+            set
+            {
+                if (value < 50 || value > 1000)
+                    throw new Exception("nem kerülhet ennyibe");
+                _ar = value;
+            }
+        }
+    }
+}
---
+++ CA201118/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA201118
+{
+    class Program
+    {
+        static List<Palinka> palinkak;
+        static readonly Random rnd = new Random();
+        static readonly string[] gyumolcsok = 
+            { "szilva", "barack", "dió", "körte", "alma", "banán", "agave" };
+        static void Main()
+        {
+            ListaFeltolt(20);
+            MindenKiir();
+            //OsszesPalinka();
+            //EladasTeszt(50);
+            Console.WriteLine("done!");
+            Console.ReadKey();
+        }
+
+        private static void MindenKiir()
+        {
+            foreach (var p in palinkak)
+            {
+                Console.WriteLine(
+                    $"{p.Gyumolcs} pálnaka ({p.AlkoholFok}%): " +
+                    $"\n\tKészült:   {p.KeszitesEve}" +
+                    $"\n\tKészleten: {p.Mennyiseg / 10.0} liter" +
+                    $"\n\tÁr:        {p.Ar} Ft/dl\n");
+            }
+        }
+
+        private static void ListaFeltolt(int db)
+        {
+            palinkak = new List<Palinka>();
+            for (int i = 0; i < db; i++)
+            {
+                palinkak.Add(
+                    new Palinka()
+                    {
+                        AlkoholFok = rnd.Next(30, 88),
+                        Gyumolcs = gyumolcsok[rnd.Next(gyumolcsok.Length)],
+                        Mennyiseg = rnd.Next(51),
+                        KeszitesEve = rnd.Next(2000, DateTime.Now.Year + 1),
+                        Ar = rnd.Next(50, 1001),
+                    });
+            }
+        }
+
+        private static void OsszesPalinka()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void EladasTeszt(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
